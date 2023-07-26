022d4b74a36c1c193275d6bf07486c53a3bf7452
auyeros <auyerosignacio@gmail.com>
2023-07-26 05:07:15 -0300

    show message logic

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -12,17 +12,29 @@
 
 import React, { useState } from "react";
 
-const [isParagraphVisible, setIsParagraphVisible] = useState(false);
-
-const handleClick = () => {
-	setIsParagraphVisible((prevIsParagraphVisible) => !prevIsParagraphVisible);
-};
-
 export function Message() {
+	const [isParagraphVisible, setIsParagraphVisible] = useState(false);
+
+	const handleClick = () => {
+		setIsParagraphVisible((prevIsParagraphVisible) => !prevIsParagraphVisible);
+	};
+
 	return (
+		// anchor error can be fixed using a button and if needed can be styled to look like a anchor element using:
+		// .link-button {
+		// 	background-color: transparent;
+		// 	border: none;
+		// 	cursor: pointer;
+		// 	text-decoration: underline;
+		// 	display: inline;
+		// 	margin: 0;
+		// 	padding: 0;
+		// }
 		<>
-			<a href="#">Want to buy a new car?</a>
-			<p>Call +11 22 33 44 now!</p>
+			<a href="#" onClick={handleClick} role="button" tabIndex={0}>
+				Want to buy a new car?
+			</a>
+			{isParagraphVisible && <p>Call +11 22 33 44 now!</p>}
 		</>
 	);
 }
